eb7e25d17da9bd5fc9181fecc7e3bb9002ddd1e6
narutomaksim <maksim.lyakh.2000@mail.ru>
2019-08-13 18:02:58 +0300

    version1

--- src/Header.js
+++ src/Header.js
@@ -7,6 +7,7 @@ import './bootstrap.css';
 
 class Header extends Component {
   render() {
+    const url = "/";
     return (
       <div className="container">
           <div className="App">
@@ -15,7 +16,8 @@ class Header extends Component {
         <Row>
           <Col span={8}>
               <div style={{backgroundColor: "#8fd2ff"}}>
-                  <p>Регистрация</p>
+
+                  <p><a className="big_size" href={url}>Регистрация</a></p>
                   <p>Авторизация</p>
               </div>
           </Col>
