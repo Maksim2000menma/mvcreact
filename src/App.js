a6037ce29d09efded1cc003a18753413db7291c5
narutomaksim <maksim.lyakh.2000@mail.ru>
2019-08-13 16:47:37 +0300

    test react component antd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import Button from 'antd/es/button';
 import { Icon } from 'antd';
+import { Typography } from 'antd';
+import { Row, Col } from 'antd';
 import './App.css';
 
+const { Text } = Typography;
 
 class App extends Component {
   render() {
@@ -10,9 +13,29 @@ class App extends Component {
       <div className="App">
         <h1>Контент сайта</h1>
         <Button type="primary">Button</Button>
+        <br />
         <Button type="primary" shape="circle" icon="search" />
+        <br />
         <Icon type="mobile" />
         { /* Комментарий*/}
+        <h1><Text type="warning">Просто текст</Text></h1>
+        <Text mark>Ant Design</Text>
+
+        <div style={{ backgroundColor: "#44014C", width: "300px", minHeight: "200px"}}>
+        <h1>Привет</h1>
+        </div>
+
+        <div>
+          <Row>
+            <Col span={12}>
+            <div>
+            col-12
+            </div>
+            </Col>
+            <Col span={12}>col-12</Col>
+          </Row>
+        </div>
+        
       </div>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'antd';
 import './App.css';
+import './bootstrap.css';
 
 
 class Header extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Шапка сайта</h1>
+      <div className="container">
+          <div className="App">
+              <h1>Шапка сайта</h1>
+          </div>
+        <Row>
+          <Col span={8}>
+              <div style={{backgroundColor: "#8fd2ff"}}>
+                  <p>Регистрация</p>
+                  <p>Авторизация</p>
+              </div>
+          </Col>
+
+          <Col span={15} offset={1}>
+              <div style={{backgroundColor: "#8fd23f"}}>
+                  col-12
+              </div>
+          </Col>
+        </Row>
+
       </div>
     );
   }
