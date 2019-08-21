d2c487b62c4983a7b6f0d04a47c8dbc2e18d004a
narutomaksim <maksim.lyakh.2000@mail.ru>
2019-08-21 16:58:11 +0300

    normal work put

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -9,6 +9,7 @@ class Static extends React.Component {
      super(props);
      this.state = {
        lists: [],
+       id: '',
        last_name: '',
        first_name: '',
        login: '',
@@ -26,6 +27,7 @@ class Static extends React.Component {
        this.onChangeAddress = this.onChangeAddress.bind(this);
        this.onChangeDateB = this.onChangeDateB.bind(this);
        this.onChangeRole = this.onChangeRole.bind(this);
+       this.onChangeId = this.onChangeId.bind(this);
        this.onSubmit = this.onSubmit.bind(this);
   }
 
@@ -43,38 +45,25 @@ class Static extends React.Component {
   }
 
   onSubmit(event){
-    event.preventDefault();
-    //var url = document.URL;
-    //const id = url.substr(url.lastIndexOf('/') + 1);
+    event.preventDefault();//норм url
     const {idUser} = this.props.match.params;
 
-
-    // axios.put(`http://backmyapp/user/edit/${idUser}`,{
-    //   submitapp:'Подтвердить',
-    //   last_name:'good',
-    //   first_name: 'good',
-    //   login: 'good',
-    //   password: 'good',
-    //   date_b: '2019-08-01',
-    //   address: 'good',
-    //   role_id: '3',
-    //   description: 'good'
-    // })
         axios.put(`http://backmyapp/user/edit/${idUser}`,{
-          submitapp:'Подтвердить',
-          last_name:'good',
-          first_name: 'good',
-          login: 'good',
-          password: 'good',
-          date_b: '2019-08-01',
-          address: 'good',
-          role_id: '3',
-          description: 'good'
-        })
+          id:this.state.id,
+          submitapp:'good',
+          last_name:this.state.last_name,
+          first_name: this.state.first_name,
+          login: this.state.login,
+          password: this.state.password,
+          date_b: this.state.date_b,
+          address: this.state.address,
+          role_id: this.state.role_id,
+          description: this.state.description
+        },{headers: {"Content-Type": "text/plain"}})
           .then(response => {
           const lists = response.data;
           console.log(response.data);
-            this.setState({ lists });
+          //this.setState({ lists });
         })
     alert('конец');
   }
@@ -108,6 +97,9 @@ class Static extends React.Component {
   onChangeRole(event) {
     this.setState({role_id: event.target.value});
   }
+  onChangeId(event) {
+    this.setState({id: event.target.value});
+  }
 
 
   render() {
@@ -116,30 +108,33 @@ class Static extends React.Component {
     <br / >
     <center><h1>Редактирование информации</h1></center>
     <br />
-          { this.state.lists.map(list =>
+
             <div>
+               {this.state.lists.map(list =>
+                  <div>
+                      {list.last_name}
 
-            <form onSubmit={this.onSubmit}>
-            <Descriptions title="Информация о пользователе" bordered>
-              <Descriptions.Item label="ID">{list.id}</Descriptions.Item>
-              <Descriptions.Item label="Last name"><input type="text" name="last_name" value={this.state.last_name} onChange={this.onChangeLast_name}/></Descriptions.Item>
-              <Descriptions.Item label="First name"><input type="text" name="first_name" value={this.state.first_name} onChange={this.onChangeFirst_name}/></Descriptions.Item>
-              <Descriptions.Item label="Login"><input type="text" name="login" value={this.state.login} onChange={this.onChangeLogin}/></Descriptions.Item>
-              <Descriptions.Item label="Password" span={2}><input type="text" name="password" value={this.state.password} onChange={this.onChangePassword}/></Descriptions.Item>
-              <Descriptions.Item label="Date"><input type="date" name="date_b" value={this.state.date_b} onChange={this.onChangeDateB}/></Descriptions.Item>
-              <Descriptions.Item label="Address"><input type="text" name="address" value={this.state.address} onChange={this.onChangeAddress}/></Descriptions.Item>
-              <Descriptions.Item label="Role"><input type="text" name="role_id" value={this.state.role_id} onChange={this.onChangeRole}/></Descriptions.Item>
-              <Descriptions.Item label="Description"><input type="text" name="description" value={this.state.description} onChange={this.onChangeDescription}/></Descriptions.Item>
-            </Descriptions>
 
-            <br />
-            <input type="submit" name="submitapp" value="Подтвердить" />
-            </form>
+                <form onSubmit={this.onSubmit}>
+                  <Descriptions title="Информация о пользователе" bordered>
+                    <Descriptions.Item label="ID"><input type="text" name="id" value={this.state.id} onChange={this.onChangeId} placeholder={list.id}/></Descriptions.Item>
+                    <Descriptions.Item label="Last name"><input type="text" name="last_name" value={this.state.last_name} onChange={this.onChangeLast_name} placeholder={list.last_name}/></Descriptions.Item>
+                    <Descriptions.Item label="First name"><input type="text" name="first_name" value={this.state.first_name} onChange={this.onChangeFirst_name} placeholder={list.first_name}/></Descriptions.Item>
+                    <Descriptions.Item label="Login"><input type="text" name="login" value={this.state.login} onChange={this.onChangeLogin} placeholder={list.login}/></Descriptions.Item>
+                    <Descriptions.Item label="Password" span={2}><input type="text" name="password" value={this.state.password} onChange={this.onChangePassword} placeholder={list.password}/></Descriptions.Item>
+                    <Descriptions.Item label="Date"><input type="date" name="date_b" value={this.state.date_b} onChange={this.onChangeDateB} placeholder={list.datу
+                    }/></Descriptions.Item>
+                    <Descriptions.Item label="Address"><input type="text" name="address" value={this.state.address} onChange={this.onChangeAddress} placeholder={list.address}/></Descriptions.Item>
+                    <Descriptions.Item label="Role"><input type="text" name="role_id" value={this.state.role_id} onChange={this.onChangeRole} placeholder={list.role_id}/></Descriptions.Item>
+                    <Descriptions.Item label="Description"><input type="text" name="description" value={this.state.description} onChange={this.onChangeDescription} placeholder={list.description}/></Descriptions.Item>
+                  </Descriptions>
+                  <input type="submit" name="submitapp" value="Подтвердить" />
+                </form>
 
-            <br />
-            <br />
+                </div>
+              )}
             </div>
-          ) }
+
     </div>
   )
 }
